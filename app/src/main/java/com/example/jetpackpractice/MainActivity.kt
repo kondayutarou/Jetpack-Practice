1e03de94a342c71309167b319329ca4484188a68
YUTARO KONDA <kondayutarou@gmail.com>
2022-02-12 20:00:05 +0900

    fix

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -3,38 +3,41 @@ package com.example.jetpackpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackpractice.ui.theme.JetpackPracticeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackPracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            GreetingText(name = "world")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(name: String) {
+    Text(
+        text = " Hello $name!",
+        modifier = Modifier
+            .width(200.dp)
+            .height(240.dp)
+            .clickable(onClick = { })
+            .padding(all = 24.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackPracticeTheme {
-        Greeting("Android")
-    }
+fun DefaultPreviewMainActivity() {
+    GreetingText(name = "world")
 }
+
