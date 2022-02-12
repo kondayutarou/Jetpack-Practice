dcd0d7a481081809a62ea24858538c89a4c35f28
YUTARO KONDA <kondayutarou@gmail.com>
2022-02-12 21:31:13 +0900

    fix

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -7,11 +7,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -28,10 +33,12 @@ fun GreetingText(name: String) {
     Text(
         text = " Hello $name!",
         modifier = Modifier
-            .width(200.dp)
-            .height(240.dp)
             .clickable(onClick = { })
             .padding(all = 24.dp)
+            .width(200.dp)
+            .height(240.dp),
+        style = MaterialTheme.typography.button,
+        fontWeight = FontWeight.SemiBold
     )
 }
 
